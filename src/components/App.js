4923febc04d0267a1f2df0c70c1c3b7c73020605
Juan Aguilera <jefraix@gmail.com>
2023-08-12 19:06:55 -0400

    Added other routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
 import { connect } from "react-redux";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import SignIn from "./SignIn";
 import Home from "./Home";
+import Leaderboard from "./Leaderboard";
+import PollAdd from "./PollAdd";
+import NavigationBar from "./NavigationBar";
+import { Fragment } from "react";
 
 const App = (props) => {
+  const location = useLocation();
   return (
     <div className="App">
       {props.loading === true ? (
         <p className="loading-text">Loading...</p>
       ) : (
-        <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/" exact element={<Home />} />
-        </Routes>
+        <Fragment>
+          {location.pathname !== "/signin" && <NavigationBar />}
+          <Routes>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/" exact element={<Home />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/add" element={<PollAdd />} />
+          </Routes>
+        </Fragment>
       )}
     </div>
   );
