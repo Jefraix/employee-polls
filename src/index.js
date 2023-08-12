e72067ffece3b9122e6b7163e9e076667e024e13
Juan Aguilera <jefraix@gmail.com>
2023-08-11 23:16:43 -0400

    Installed react router, now fetching users

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import { legacy_createStore as createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
-
+import middleware from "./middleware";
+import "./index.css";
 import App from "./components/App";
+import { handleGetUsers } from "./actions/users";
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
+store.dispatch(handleGetUsers());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
